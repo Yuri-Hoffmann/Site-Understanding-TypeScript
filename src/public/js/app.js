bc688a6600f7dbac6f04ca8cc34c0480807fe4b1
Yuri-Hoffmann <120231510+Yuri-Hoffmann@users.noreply.github.com>
2023-01-30 17:06:34 -0400

    Delete src/public directory

--- src/public/js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Department {
-    constructor(n) {
-        this.name = n;
-    }
-}
-const accouting = new Department('Accounting');
-console.log(accouting);
-//# sourceMappingURL=app.js.map
