11cd4c384b22390e1139b3f1008b4e2abbb157cf
Yuri-Hoffmann <120231510+Yuri-Hoffmann@users.noreply.github.com>
2023-01-30 17:03:51 -0400

    Add files via upload

---
+++ src/public/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accouting = new Department('Accounting');
+console.log(accouting);
+//# sourceMappingURL=app.js.map
